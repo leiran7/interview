826af3b1166bbed7aa86d5f6ba58a437f0382b7e
leran sobih <scolern7@gmail.com>
2022-12-26 20:59:25 +0200

    last changes

--- src/scripts/initServer.js
+++ src/scripts/initServer.js
@@ -1,7 +1,5 @@
-const { initSequlize } = require('../components/sequelize');
 const { initExpress } = require('../components/express');
 
 module.exports = async (expressApp) => {
-  await initSequlize();
   await initExpress(expressApp);
 };
